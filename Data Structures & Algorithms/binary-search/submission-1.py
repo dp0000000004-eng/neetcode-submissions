552a8f84f96f117783f6a20390b1a492b202d105
Debasish Pradhan <dp0000000004@gmail.com>
2026-07-21 19:25:53 +0530

    Delete Data Structures & Algorithms/binary-search/submission-1.py

--- Data Structures & Algorithms/binary-search/submission-1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        for num in nums:
-            if num == target:
-                return nums.index(num)
-        else:
-            return -1
