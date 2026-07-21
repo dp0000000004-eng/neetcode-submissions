686c28ca1a01c0c76e7b2117f236afc55b7f73f8
Debasish Pradhan <dp0000000004@gmail.com>
2026-07-21 19:24:55 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        
+
+        for i , num in enumerate(nums):
+            
+            if num == target:
+                return i
+        
+        else:
+            return -1
